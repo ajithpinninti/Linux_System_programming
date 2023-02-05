93b03b773eacf69e41fd43c99a3e89e615bc4d5c
ajithpinninti <ajitpinninti@gmail.com>
2023-02-05 08:23:08 +0530

    syslog file

---
+++ syslog_check.c
@@ -0,0 +1,18 @@
+#include<syslog.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE *f;
+
+    f = fopen("not_here","r");
+
+    if(f)
+        syslog(LOG_ERR|LOG_USER,"ooops - %m\n");
+ 
+    printf("completed");
+    
+    
+    exit(0);
+}
